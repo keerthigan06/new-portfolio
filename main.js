3af82dd2c624e8f86849a8156887bcf4f3533a9f
keerthigan06 <muhunthankeerthigan@gmail.com>
2023-12-28 21:18:57 +0530

    new portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#home .text", {
+    strings: ["Graphic Designer","UI and UX Developer" , "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
