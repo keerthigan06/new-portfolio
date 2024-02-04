aef1681a7aedb753afc2409c5a2b154ebf8ebb8a
keerthigan06 <muhunthankeerthigan@gmail.com>
2024-02-04 22:17:03 +0530

    update media query mail btn

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#home .text", {
-    strings: ["Graphic Designer","UI and UX Developer" , "Web Developer"],
+    strings: ["Graphic Designer","UI and UX Developer" , "Web Developer","Software engineer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
